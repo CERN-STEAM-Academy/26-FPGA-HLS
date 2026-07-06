8ad4928d959cd49a678168e9f8db98016f5ef9ee
Sioni Summers <14807534+thesps@users.noreply.github.com>
2026-07-06 11:52:27 +0200

    Add csim and cosim scripts for pynq part

---
+++ pynq/cosim.tcl
@@ -0,0 +1,26 @@
+# Project
+open_project -reset "METProj"
+add_files -tb ../MET/pt.csv
+add_files -tb ../MET/phi.csv
+add_files -tb testbench.cpp
+# TODO replace with your implementation
+add_files example_met.cpp
+add_files met_pynq.cpp
+set_top met_accelerator
+
+# Solution
+open_solution -reset "solution"
+set_part {xc7z020clg400-1}
+create_clock -period 10.0 -name default
+
+# Simulation
+csim_design -ldflags "-fuse-ld=gold"
+
+# Synthesis
+csynth_design
+
+# Co-simulation
+cosim_design -ldflags "-fuse-ld=gold"
+
+exit
+
