1f98408f4abe86cc33bc7095c5c06466dad93c0a
Sioni Summers <14807534+thesps@users.noreply.github.com>
2026-07-10 15:26:21 +0200

    Implement load-compute-store paradigm for pynq part

--- pynq/met_pynq.cpp
+++ pynq/met_pynq.cpp
@@ -1,35 +1,76 @@
 // TODO include your own implementation here
-#include "dummy_met.h"
+#include "example_met.h"
+#include "hls_stream.h"
 
 // this defines the size of internal buffer that's used
 static constexpr int MAX_N_PARTICLES = 256;
 
-/* Function to copy the particles of one event into internal buffer,
+/* Function to copy the particles of one event from the external memory into internal streams,
 *  and cast them to internal data type used in MET calculation
 */
-void copy_inputs(int particles_start,                  // start index of event in external memory
-                 int n_particles,                      // number of particles in the event
-                 float* pt_in, float* phi_in,          // float (pT, phi) read from external memory
-                 T_pt* pt_int, T_phi* phi_int){        // locally buffered (pT, phi) in different types
-  for(unsigned short n = 0; n < n_particles; n++){
-    pt_int[n] = pt_in[particles_start + n];   // there is an implicit cast from float to T_pt here
-    phi_int[n] = phi_in[particles_start + n]; // there is an implicit cast from float to T_phi here
+void load(// these are the input arrays from external memory
+          int n_events,
+          unsigned short* n_particles,
+          float* pt,
+          float* phi, 
+          // the following are streams that will be passed to the next function internally
+          hls::stream<unsigned short>& n_particles_stream,
+          hls::stream<T_pt>& pt_stream,
+          hls::stream<T_phi>& phi_stream){
+  unsigned short particle_idx = 0; // events can have different numbers of particles, so keep track of the index here
+  for(int n_event = 0; n_event < n_events; n_event++){
+    unsigned short n_particles_event = n_particles[n_event];
+    n_particles_stream.write(n_particles_event);
+    for(unsigned short n = 0; n < n_particles_event; n++, particle_idx++){
+      T_pt the_pt = (T_pt) pt[particle_idx]; // read from the external memory and cast from float to T_pt
+      T_phi the_phi = (T_phi) phi[particle_idx]; // read from the external memory and cast from float to T_phi
+      pt_stream.write(the_pt);
+      phi_stream.write(the_phi);
+    }
+  }
+}
+
+/* Function to compute the MET over all events
+*  It reads from the particle stream to an internal buffer, and calls your compute_MET function
+*/
+void compute(int n_events,
+             hls::stream<unsigned short>& n_particles_stream, 
+             hls::stream<T_pt>& pt_stream,
+             hls::stream<T_phi>& phi_stream,
+             hls::stream<T_met>& met_stream){
+  // internal buffers for particles in one event
+  T_pt pt_int[MAX_N_PARTICLES];
+  T_phi phi_int[MAX_N_PARTICLES];
+
+  for(int n_event=0; n_event < n_events; n_event++){
+    unsigned short n_particles_event = n_particles_stream.read();
+    for(unsigned short n=0; n < n_particles_event; n++){
+      pt_int[n] = pt_stream.read();
+      phi_int[n] = phi_stream.read();
+    }
+    T_met met_int = compute_met(n_particles_event, pt_int, phi_int);
+    met_stream.write(met_int);
   }
 }
 
 /* Function to copy the MET of one event into the external memory,
 *  and cast it from the internal MET type to float
 */
-void copy_outputs(int n_event, T_met met_int, float* met_out){
-  met_out[n_event] = met_int; // there is an implicit cast from T_met to float here
+void store(int n_events,
+           hls::stream<T_met>& met_stream,
+           float* met){
+  for(int n = 0; n < n_events; n++){
+    T_met the_met = met_stream.read(); // read from the internal stream
+    met[n] = (float) the_met;          // cast to float and write to the external memory
+  }
 }
 
 void met_accelerator(int n_events, unsigned short* n_particles, float* pt, float* phi, float* met){
   // data interfaces
-  #pragma HLS interface mode=m_axi port=n_particles offset=slave bundle=gmem0
-  #pragma HLS interface mode=m_axi port=pt offset=slave bundle=gmem0
-  #pragma HLS interface mode=m_axi port=phi offset=slave bundle=gmem0
-  #pragma HLS interface mode=m_axi port=met offset=slave bundle=gmem0
+  #pragma HLS interface mode=m_axi port=n_particles offset=slave bundle=gmem0 depth=2532
+  #pragma HLS interface mode=m_axi port=pt offset=slave bundle=gmem0 depth=2532
+  #pragma HLS interface mode=m_axi port=phi offset=slave bundle=gmem0 depth=2532
+  #pragma HLS interface mode=m_axi port=met offset=slave bundle=gmem0 depth=2532
   // control interfaces
   #pragma HLS interface mode=s_axilite port=n_events bundle=control
   #pragma HLS interface mode=s_axilite port=n_particles bundle=control
@@ -40,19 +81,12 @@ void met_accelerator(int n_events, unsigned short* n_particles, float* pt, float
 
   #pragma HLS dataflow
 
-  // internal buffers for particles in one event
-  T_pt pt_int[MAX_N_PARTICLES];
-  T_phi phi_int[MAX_N_PARTICLES];
+  hls::stream<unsigned short> n_particles_stream;
+  hls::stream<T_pt> pt_stream;
+  hls::stream<T_phi> phi_stream;
+  hls::stream<T_met> met_stream;
 
-  // events have different numbers of particles so we keep track of the index
-  int n_particle = 0;
-  // loop over events with 
-  for(int n_event = 0; n_event < n_events; n_event++){
-    #pragma HLS pipeline
-    unsigned short n_particles_event = n_particles[n_event];
-    copy_inputs(n_particle, n_particles_event, pt, phi, pt_int, phi_int);
-    T_met met_int = compute_met(n_particles_event, pt_int, phi_int);
-    copy_outputs(n_event, met_int, met);
-    n_particle += n_particles_event;
-  }
+  load(n_events, n_particles, pt, phi, n_particles_stream, pt_stream, phi_stream);
+  compute(n_events, n_particles_stream, pt_stream, phi_stream, met_stream);
+  store(n_events, met_stream, met);
 }
